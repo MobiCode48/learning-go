b057e79d9e118772b617b37fd933f11f4a2dae4a
MobiCode48 <emmanuelmanu544@gmail.com>
2024-12-30 13:46:48 +0100

    if statements for go learning

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -2,15 +2,37 @@ package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	if kind == "car" || kind == "truck" {
+		return true
+	}
+	return false
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
-func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+func ChooseVehicle(option1 string, option2 string) string {
+	better := " is clearly the better choice."
+	var test string
+	if option1 < option2 {
+		test := option1 + better
+		return test
+	}
+	if option2 < option1 {
+		test := option2 + better
+		return test
+	}
+	return test
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
-func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+func CalculateResellPrice(originalPrice float64, age float64) float64 {
+	var resellPrice float64
+	if age < 3 {
+		resellPrice = originalPrice * 80 / 100
+	} else if age >= 10 {
+		resellPrice = originalPrice * 50 / 100
+	} else {
+		resellPrice = originalPrice * 70 / 100
+	}
+
+	return resellPrice
 }
