6d1d381c9fff2a9eeecd802797b93008379262c9
MobiCode48 <emmanuelmanu544@gmail.com>
2025-01-05 01:13:40 +0100

    for loops

--- animal-magic/animal_magic.go
+++ animal-magic/animal_magic.go
@@ -1,16 +1,22 @@
 package chance
 
+import "math/rand"
+
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
-	panic("Please implement the RollADie function")
+	return rand.Intn(20) + 1
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	return rand.Float64() * 12
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	s := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+	rand.Shuffle(len(s), func(i, j int) {
+		s[i], s[j] = s[j], s[i]
+	})
+	return s
 }
